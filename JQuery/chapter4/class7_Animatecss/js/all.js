5b30dbfdfe4d4f88b7e206266d5026cb42b2961d
kinmio <w51700520@gmail.com>
2021-02-01 16:25:55 +0800

    加入動畫效果

--- JQuery/chapter4/class7_Animatecss/js/all.js
+++ JQuery/chapter4/class7_Animatecss/js/all.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){ /*js起手語法，確保載入jQuery*/
-    $('.start').click(function () { 
-        $('.box').stop().slideToggle(5000);
-        /*jQuery連結特性可以把動畫效果寫在一起*/
-        $('.box2').stop().fadeToggle(5000);
+    $('.open').click(function (e) { 
+        $('.box').toggleClass('animate__animated animate__flash');
     });
 }); 
