beda238f1b0799b9f79142f770464f44158af90c
kinmio <w51700520@gmail.com>
2021-03-09 15:08:20 +0800

    lightbox,swiper api應用

---
+++ JQuery/chapter7/class2-lightbox-b/js/all.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ /*js起手語法，確保載入jQuery*/
+    lightbox.option({/*lightbox 效果*/ 
+        'resizeDuration': 200,
+        'wrapAround': true,
+        'fadeDuration':600
+    });
+}); 
---
+++ JQuery/chapter7/class5-swiper-c/js/all.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){ /*js起手語法，確保載入jQuery*/
+    const swiper = new Swiper('.swiper-container', { /*new add swiper*/
+        // Optional parameters
+        direction: 'horizontal',/*決定方向*/
+        loop: true,/*是否循環撥放*/
+        effect:'fade',//滑動效果
+        longSwipesMs:3000,//
+        autoplay: {//自動撥放
+          delay: 1500,
+        },
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+}); 
